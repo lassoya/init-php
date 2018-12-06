7a228ca5aeabead0b17b08be1eb2260eca8bb664
Samuel Lassoie <samuel.lassoie@gmail.com>
2018-12-06 19:26:05 +0100

    form contact

--- formulaire-contact.php
+++ formulaire-contact.php
@@ -11,10 +11,20 @@ if($_POST) {
     $prenom = $_POST['prenom'];
   }
 */
-  $nom = isset($_POST['nom']) ? $_POST['nom'] : '';
-  $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : '';
+  $nom = isset($_POST['nom']) && !empty($_POST['nom']) ? $_POST['nom'] : 'N/A';
+  $prenom = isset($_POST['prenom']) && empty($_POST['prenom']) == false ? $_POST['prenom'] : 'N/A';
 
-  echo $nom . ' ' . $prenom;
+  $date_de_naissance = 'N/A';
+  if(isset($_POST['date_de_naissance']) && empty($_POST['date_de_naissance']) == false){
+      $date = $_POST['date_de_naissance'];
+      //1er solution
+      //$date_de_naissance = date('d/m/Y', strtotime($date));
+      //2eme solution
+      $date_de_naissance = date_format(date_create($date), 'd/m/Y');
+  }
+
+
+  echo $nom . ' ' . $prenom. ' '. $date_de_naissance;
 }
 
  ?>
@@ -27,6 +37,11 @@ if($_POST) {
     <label for="prenom">Prénom</label>
     <input id="prenom" type="text" name="prenom">
   </div>
+  <div>
+    <label for="date_de_naissance">Date de naissance</label>
+    <input id="date_de_naissance" type="date" name="date_de_naissance">
+  </div>
+  
   <div>
     <button type="reset">Reset</button>
     <button type="submit">Envoyer</button>
