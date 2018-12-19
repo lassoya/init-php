aa0e9da32e5b4937b3688934bf670c66fd056ad7
Samuel Lassoie <samuel.lassoie@gmail.com>
2018-12-19 20:34:34 +0100

    wip

--- formulaire-contact.php
+++ formulaire-contact.php
@@ -1,7 +1,7 @@
 <?php
 //EXERCICE n°1
 $civilite = ['Monsieur', 'Madame', 'Mademoiselle', 'Non-binaire'];
-
+$hobby = ['voyage', 'lecture', 'sport', 'natation'];
 //if(count($_POST)>0)
 if($_POST) {
 /*
@@ -28,55 +28,85 @@ if($_POST) {
   $taille = isset($_POST['taille']) && empty($_POST['taille']) == false ? $_POST['taille'] : 'N/A';
 
 
-  echo $nom . ' ' . $prenom. ' '. $date_de_naissance. ' ' .$taille;
+//  echo $nom . ' ' . $prenom. ' '. $date_de_naissance. ' ' .$taille;
 }
-
- ?>
-
-<form action="" method="POST">
-  <div>
-    <label>Civilité</label>
-<?php
-    //EXERCICE n°2
-    foreach($civilite as $value) {
-      echo '<input type="radio" '.
-      (isset($_POST['civilite']) && $_POST['civilite'] == $value  ? 'checked' : '' ).' name="civilite" value="' . $value . '"> '. $value;
-    }
 ?>
+<html>
+  <head>
+    <style></style>
+  </head>
+  <body>
+  <form action="" method="POST">
+    <div>
+      <label>Civilité</label>
+  <?php
+      //EXERCICE n°2
+      foreach($civilite as $value) {
+        echo '<input type="radio" '.
+        (isset($_POST['civilite']) && $_POST['civilite'] == $value  ? 'checked' : '' ).' name="civilite" value="' . $value . '"> '. $value;
+      }
+  ?>
 
-  </div>
-  <div>
-    <label for="nom" >Nom</label>
-    <input value="<?= isset($_POST['nom']) ? $_POST['nom'] : '' ?>" id="nom" type="text" name="nom">
-  </div>
-  <div>
-    <label for="prenom">Prénom</label>
-    <input value="<?= isset($_POST['prenom']) ? $_POST['prenom'] : '' ?>" id="prenom" type="text" name="prenom">
-  </div>
-  <div>
-    <label for="date_de_naissance">Date de naissance</label>
-    <input id="date_de_naissance" value="<?= isset($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : '' ?>" type="date" name="date_de_naissance">
-  </div>
-  <div>
-    <label for="taille">Taille</label>
-    <select name="taille">
-      <?php
-        for($i=0; $i<=200; $i++) {
-          echo "<option ". (isset($_POST['taille']) && $i== $_POST['taille'] ? 'selected': '')." value='$i'>$i cm</option>";
-        }
-       ?>
-    </select>
     </div>
+    <div>
+      <label for="nom" >Nom</label>
+      <input value="<?= isset($_POST['nom']) ? $_POST['nom'] : '' ?>" id="nom" type="text" name="nom">
+    </div>
+    <div>
+      <label for="prenom">Prénom</label>
+      <input value="<?= isset($_POST['prenom']) ? $_POST['prenom'] : '' ?>" id="prenom" type="text" name="prenom">
+    </div>
+    <div>
+      <label for="date_de_naissance">Date de naissance</label>
+      <input id="date_de_naissance" value="<?= isset($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : '' ?>" type="date" name="date_de_naissance">
+    </div>
+    <div>
+      <label for="taille">Taille</label>
+      <select name="taille">
+        <?php
+          for($i=0; $i<=200; $i++) {
+            echo "<option ". (isset($_POST['taille']) && $i== $_POST['taille'] ? 'selected': '')." value='$i'>$i cm</option>";
+          }
+         ?>
+      </select>
+      </div>
+
+      <div>
+        <label for="message">Message</label>
+        <textarea id="message" name="message">
+          <?= isset($_POST['message']) ? $_POST['message'] : '' ?>
+        </textarea>
+      </div>
+
+      <div>
+        <label for="hobby">Hobby</label>
+        <select name="hobby" id="hobby">
+        <?php
+          foreach($hobby as $value) {
+            echo "<option value='$value'>$value</option>";
+          }
+          //for($i=0; $i<count($hobby);$i++){
+          //  echo "<option value='".$hobby[$i]."'>".$hobby[$i]."</option>";
+          //}
+
+         ?>
+       </select>
+      </div>
 
     <div>
-      <label for="message">Mesage</label>
-      <textarea id="message" name="message">
-        <?= isset($_POST['message']) ? $_POST['message'] : '' ?>
-      </textarea>
+      <button type="reset">Reset</button>
+      <button type="submit">Envoyer</button>
     </div>
+  </form>
 
-  <div>
-    <button type="reset">Reset</button>
-    <button type="submit">Envoyer</button>
+<?php if($_POST): ?>
+  <div style="border: 1px solid #CCC; background: #EEE">
+    <?php
+      foreach($_POST as $key => $value):
+        echo '<strong>' . $key . '</strong> : ' .$value . '<br>';
+      endforeach;
+     ?>
   </div>
-</form>
+<?php endif ?>
+</body>
+</html>
