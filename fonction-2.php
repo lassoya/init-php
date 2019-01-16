1685733ef9de805cceecc842dc98a5ee3cd7b607
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-01-16 18:47:18 +0100

    exo 1

--- fonction-2.php
+++ fonction-2.php
@@ -18,3 +18,17 @@ function noms(): array {
 $noms = noms();
 
 print_r($noms[1]);
+
+
+function compter(int $nb1, int $nb2): array {
+  $tab = [];
+  //$tab = array();
+  for($nb1; $nb1<=$nb2;$nb1++){
+    $tab[] = $nb1;
+  }
+
+  return $tab;
+}
+
+
+echo implode(', ', compter(0, 10));
