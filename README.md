07b7dcad7cf25f0aee2768871e4d0335566dac9d
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-01-27 08:45:43 +0100

    exo 30/01

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [Exercices 12/12/2018](doc/exo_2018_12_12.md)
 - [Exercices 19/12/2018](doc/exo_2018_12_19.md)
 - [Exercices 16/01/2019](doc/exo_2019_01_16.md)
+- [Exercices 30/01/2019](doc/exo_2019_01_30.md)
---
+++ exo/exo-2.php
@@ -0,0 +1,24 @@
+<?php
+
+//EXO n°1
+$nom = [
+  'Toto',
+  'Roger',
+  'Maurice',
+  'David',
+  'Veronique',
+  'Toto',
+  'René',
+  'Roger';
+];
+
+
+//EXO N°2
+$fruit = [
+      'Banane',
+      'Pomme',
+      'Poire',
+      'Cerise',
+      'Fraise',
+      'Framboise'
+    ];
