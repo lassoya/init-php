36634e2afa51c6251c6978be1201ac6ba3d90232
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-02-13 21:20:13 +0100

    wip

--- session/login.php
+++ session/login.php
@@ -1,3 +1,25 @@
+<?php
+session_start();
+
+$username= 'etudiant';
+$password = '1234';
+
+if(isset($_GET['deconnexion'])){
+  unset($_SESSION['username']);
+  header('location: login.php');
+  exit;
+}
+
+if(isset($_POST['username'], $_POST['password'])
+&& $username == $_POST['username']
+&& $password == $_POST['password'] ){
+  $_SESSION['username'] = $_POST['username'];
+} else if($_POST) {
+  echo 'Vos identifiants ne sont pas corrects';
+}
+
+if(isset($_SESSION['username']) == false):
+?>
 <form method="post" action="">
   <div>
     <label>Username</label>
@@ -9,3 +31,8 @@
   </div>
   <input type="submit" value="Connexion">
 </form>
+<?php
+else:
+  echo "vous êtes connectés <a href='?deconnexion=true'>Déconnexion</a>";
+endif;
+ ?>
