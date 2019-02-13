5c49a37abb12249bcf68dec4f27a5773c49bba20
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-02-13 20:46:06 +0100

    wip

---
+++ session/login.php
@@ -0,0 +1,11 @@
+<form method="post" action="">
+  <div>
+    <label>Username</label>
+    <input type="text" name="username">
+  </div>
+  <div>
+    <label>password</label>
+    <input type="password" name="password">
+  </div>
+  <input type="submit" value="Connexion">
+</form>
