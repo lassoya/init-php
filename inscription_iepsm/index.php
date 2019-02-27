4e96ceab1c7d9ef4c3f6450d42ad1be9fcac7587
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-02-27 21:19:58 +0100

    wip

--- inscription_iepsm/index.php
+++ inscription_iepsm/index.php
@@ -2,4 +2,51 @@
 
 $user = 'inscription';
 $pass = 'inscription2019';
-$dbh = new PDO('mysql:host=127.0.0.1;dbname=inscription_iepsm', $user, $pass);
+try {
+  $bdd = new PDO('mysql:host=127.0.0.1;dbname=inscription_iepsm', $user, $pass);
+} catch(\Exception $error){
+  error_log($error->getMessage());
+  echo "Erreur: connexion BDD impossible.";
+}
+
+$request = $bdd->query('select * from utilisateur');
+
+//echo "<pre>";
+//print_r($request->fetchAll(PDO::PARAM_STR));
+
+?>
+
+<table border="1">
+  <thead>
+    <tr>
+      <th>id</th>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>Genre</th>
+      <th>Adresse</th>
+      <th>Code postal</th>
+      <th>Ville</th>
+      <th>Email</th>
+      <th>Téléphone</th>
+      <th>Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    
+    <tr>
+      <td>id</td>
+      <td>Nom</td>
+      <td>Prénom</td>
+      <td>Genre</td>
+      <td>Adresse</td>
+      <td>Code postal</td>
+      <td>Ville</td>
+      <td>Email</td>
+      <td>Téléphone</td>
+      <td>Type</td>
+    </tr>
+    ?>
+
+  </tbody>
+</table>
