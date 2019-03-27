5203edcac9a8e0aa8615352679a51bae09c8aed4
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-03-27 20:53:07 +0100

    wip

--- pendu/index.php
+++ pendu/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 const MAX_ESSAIS = 10;
+$_SESSION['essais'] = isset($_SESSION['essais']) ? $_SESSION['essais'] : 0;
+//$_SESSION['essais'] = $_SESSION['essais'] ?? 0;
 
 $mots  = [
   "coquelicot",
@@ -17,10 +19,33 @@ if($_SESSION['essais'] == 0) {
   $position = rand(0, count($mots)-1);
   $_SESSION['mot'] = $mots[$position];
 
-  $_SESSION['lettre_mot'] = [];  
+  $explodeWord = str_split($_SESSION['mot']);
+
+  $_SESSION['lettres_mot'] = array_unique($explodeWord);
 
   $_SESSION['essais'] = MAX_ESSAIS;
   $_SESSION['lettres_utilisees'] = [];
+} else if(isset($_POST['lettre'])) {
+  //verifier que la lettre se trouve dans le mot
+
 }
 
-echo $_SESSION['mot'];
+
+?>
+
+<form action="" method="post">
+<?php
+  $detail = [];
+  for($i=0; $i<strlen($_SESSION['mot']); $i++) {
+    $detail[] = '_';
+  }
+  echo implode($detail, ' ');
+ ?>
+  <div>
+    <label>Lettre</label>
+    <input maxlength="1" type="text" name="lettre" value="">
+  </div>
+  <div>
+    <input type="submit" value="Envoyer">
+  </div>
+</form>
