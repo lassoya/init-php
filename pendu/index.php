2bdb1758b523f72ba974d3d3102cd62591d18750
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-03-27 19:23:26 +0100

    wip

--- pendu/index.php
+++ pendu/index.php
@@ -13,11 +13,14 @@ $mots  = [
   "bijouterie"
 ];
 
+if($_SESSION['essais'] == 0) {
+  $position = rand(0, count($mots)-1);
+  $_SESSION['mot'] = $mots[$position];
 
-$position = rand(0, count($mots)-1);
-$_SESSION['mot'] = $mots[$position];
-$_SESSION['essais'] = 0;
-$_SESSION['lettres_utilisees'] = [];
+  $_SESSION['lettre_mot'] = [];  
 
+  $_SESSION['essais'] = MAX_ESSAIS;
+  $_SESSION['lettres_utilisees'] = [];
+}
 
 echo $_SESSION['mot'];
